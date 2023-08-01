087d58038886cc196625c1339e39ff5f514531ee
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2023-08-01 15:18:55 +0530

    Time: 62 ms (22.23%), Space: 81 MB (13.27%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     int helper(string str1,string str2,int ind1,int ind2,int n,int m,vector<vector<int>>&dp)
     {
-        if(ind2<0)
+        if(ind2==0)
         {
-            return ind1+1;
+            return ind1;
         }
-        else if(ind1<0)
+        else if(ind1==0)
         {
-            return ind2+1;
+            return ind2;
         }
         if(dp[ind1][ind2]!=-1)
         {
             return dp[ind1][ind2];
         }
-        if(str1[ind1]==str2[ind2])
+        if(str1[ind1-1]==str2[ind2-1])
         {
             return dp[ind1][ind2]= helper(str1,str2,ind1-1,ind2-1,n,m,dp);
         }
@@ -27,7 +27,15 @@ public:
     {
         int n=word1.length();
         int m=word2.length();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return helper(word1,word2,n-1,m-1,n,m,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        if(n==0)
+        {
+            return m;
+        }
+        if(m==0)
+        {
+            return n;
+        }
+        return helper(word1,word2,n,m,n,m,dp);
     }
 };
