a0e12e5faf47f6a8b01ab602ce4b27cab8d1909f
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2023-08-01 15:30:12 +0530

    Time: 15 ms (61.45%), Space: 9.2 MB (26.67%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -28,28 +28,27 @@ public:
         int n=str1.length();
         int m=str2.length();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        for(int i=0;i<=n;i++)
-        {
-            dp[i][0]=i;
-        }
+        vector<int>curr(m+1,0),prev(m+1,0);
         for(int j=0;j<=m;j++)
         {
-            dp[0][j]=j;
+            prev[j]=j;
         }   
         for(int ind1=1;ind1<=n;ind1++)
         {
+            curr[0]=ind1;
             for(int ind2=1;ind2<=m;ind2++)
             {
                 if(str1[ind1-1]==str2[ind2-1])
                 {
-                    dp[ind1][ind2]= dp[ind1-1][ind2-1];
+                    curr[ind2]= prev[ind2-1];
                 }
                 else
                 {
-                    dp[ind1][ind2]= 1+min(dp[ind1][ind2-1],min(dp[ind1-1][ind2-1],dp[ind1-1][ind2]));
+                    curr[ind2]= 1+min(curr[ind2-1],min(prev[ind2-1],prev[ind2]));
                 }
             }
+            prev=curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
