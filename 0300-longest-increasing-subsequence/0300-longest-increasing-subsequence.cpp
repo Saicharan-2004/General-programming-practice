7178d3731c256fb22c1e21e2ca8fcfd62699b596
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2023-08-05 19:55:16 +0530

    Time: 9 ms (90.34%), Space: 10.3 MB (84.9%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -18,25 +18,43 @@ public:
         }
         return dp[ind][prev+1]=max(take,nottake);
     }
-    int lengthOfLIS(vector<int>& nums) 
+    int lengthOfLIS(vector<int>& arr) 
     {
-        int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        vector<int>forw(n+1,0),curr(n+1,0);
-        for(int ind=n-1;ind>=0;ind--)
+        // int n=nums.size();
+        // // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        // vector<int>forw(n+1,0),curr(n+1,0);
+        // for(int ind=n-1;ind>=0;ind--)
+        // {
+        //     for(int prev=n-1;prev>=-1;prev--)
+        //     {
+        //         int ans=0;
+        //         ans=forw[prev+1];
+        //         if(prev==-1||nums[ind]>nums[prev])
+        //         {
+        //             ans=max(ans,1+forw[ind+1]);
+        //         }
+        //         curr[prev+1]=ans;
+        //     }
+        //     forw=curr;
+        // }
+        // return curr[0];
+        int len=1;
+        vector<int>ans;
+        int n=arr.size();
+        ans.push_back(arr[0]);
+        for(int i=1;i<n;i++)
         {
-            for(int prev=n-1;prev>=-1;prev--)
+            if(arr[i]>ans.back())
             {
-                int ans=0;
-                ans=forw[prev+1];
-                if(prev==-1||nums[ind]>nums[prev])
-                {
-                    ans=max(ans,1+forw[ind+1]);
-                }
-                curr[prev+1]=ans;
+                ans.push_back(arr[i]);
+                len++;
             }
-            forw=curr;
+            else
+            {
+                int lb=lower_bound(ans.begin(),ans.end(),arr[i])-ans.begin();
+                ans[lb]=arr[i];
+            }
         }
-        return curr[0];
+        return len;
     }
 };
