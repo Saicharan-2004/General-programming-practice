36e9f8d0993ff9b7484d31cdede37e90728a950f
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2023-09-01 16:30:12 +0530

    Time: 27 ms (70.26%), Space: 27.1 MB (9.55%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
@@ -1,13 +1,46 @@
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& nums) 
+    vector<int> sortedSquares(vector<int>& arr) 
     {
-        int n=nums.size();
+        int n=arr.size();
+        int ind_neg=-1;
         for(int i=0;i<n;i++)
         {
-            nums[i]=nums[i]*nums[i];
+            if(arr[i]>=0)
+            {
+                ind_neg=i;
+                break;
+            }
         }
-        sort(nums.begin(),nums.end());
-        return nums;
+        vector<int>ans;
+        if(ind_neg==-1)
+        {
+            ind_neg=n;
+        }
+        int ind=ind_neg-1;
+        while(ind>=0 && ind_neg<n)
+        {
+            if(arr[ind]*arr[ind]<=arr[ind_neg]*arr[ind_neg])
+            {
+                ans.push_back(arr[ind]*arr[ind]);
+                ind--;
+            }
+            else
+            {
+                ans.push_back(arr[ind_neg]*arr[ind_neg]);
+                ind_neg++;
+            }
+        }
+        while(ind>=0)
+        {
+            ans.push_back(arr[ind]*arr[ind]);
+            ind--;
+        }
+        while(ind_neg<n)
+        {
+            ans.push_back(arr[ind_neg]*arr[ind_neg]);
+            ind_neg++;
+        }
+        return ans;
     }
 };
