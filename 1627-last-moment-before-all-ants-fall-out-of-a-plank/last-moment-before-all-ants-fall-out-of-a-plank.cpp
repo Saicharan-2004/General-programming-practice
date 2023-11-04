4f8c8d18dc355ea99af1e5ead69bce505baca3b3
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2023-11-04 22:59:59 +0530

    Time: 23 ms (16.81%) | Memory: 23.2 MB (31.47%) - LeetSync

--- 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.cpp
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.cpp
@@ -4,13 +4,9 @@ public:
     {
         int maxi=-1;
         for(auto it:left)
-        {
             maxi=max(maxi,it);
-        }
         for(auto it:right)
-        {
             maxi=max(maxi,abs(n-it));
-        }
         return maxi;
     }
 };
