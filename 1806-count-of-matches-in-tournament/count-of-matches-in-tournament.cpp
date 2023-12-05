a033fe333e9aace987562faa948ab34018da5d5c
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2023-12-05 14:20:16 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (13.68%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
@@ -2,31 +2,12 @@ class Solution {
 public:
     int numberOfMatches(int n) 
     {
-        int cnt=0;
-        if(n%2==0)
-        {
-            while(n>1)
-            {
-                if(n%2==0)
-                 cnt=cnt+n/2;
-                else
-                    cnt=cnt+(n-1)/2+1;
-                n=n/2;
-            }
-            return cnt;
+        if(n==1)
+        {   
+            return 0;
         }
-        else
-        {
-            
-          while(n>1)
-          {
-              if(n%2==0)
-                 cnt=cnt+n/2;
-                else
-                    cnt=cnt+(n-1)/2+1;
-                n=n/2;
-          }
-            return cnt;
-        }
+        return (n-1);
+
+        
     }
 };
