fa782f2609c41b3f0303e34722a20f307c0613dd
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-01-09 08:41:34 +0530

    Time: 3 ms (66.12%) | Memory: 13.2 MB (49.18%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.cpp
+++ 904-leaf-similar-trees/leaf-similar-trees.cpp
@@ -30,17 +30,6 @@ public:
         vector<int>vec2;
         traverse(root1,vec1);
         traverse(root2,vec2);
-        if(vec1.size()!=vec2.size())
-        {
-            return false;
-        }
-        for(int i=0;i<vec1.size();i++)
-        {
-            if(vec1[i]!=vec2[i])
-            {
-                return false;
-            }
-        }
-        return true;
+        return vec1==vec2;
     }
 };
