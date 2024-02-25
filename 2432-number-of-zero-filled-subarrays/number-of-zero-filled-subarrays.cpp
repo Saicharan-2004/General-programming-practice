ba48ce2a9efe508ace2190fdcfdebd4ab1df9a15
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-02-25 11:23:45 +0530

    Time: 119 ms (61.79%) | Memory: 109.9 MB (10.83%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -1,37 +1,17 @@
 class Solution {
 public:
-    long long zeroFilledSubarray(vector<int>& arr) 
+    long long zeroFilledSubarray(vector<int>& nums) 
     {
-        int start=0;
-        int end=0;
-        int n=arr.size();
-        long long int sum=0;
-        while(end<n && start<n)
+        long long int ans=0;
+        int n=nums.size();
+        for(int i=0,j=0;i<n;i++)
         {
-            if(end<n)
+            if(nums[i]!=0)
             {
-                if(arr[end]!=0)
-                {
-                    if(arr[start]==0)
-                    {
-                        long long int it=end-start;
-                        sum+=(long long int)(((it)*(it+1))/2);
-                        start=end;
-                    }
-                    else
-                    {
-                        start++;
-                        end++;
-                    }
-                }
-                else
-                {
-                    end++;
-                }
+                j=i+1;
             }
+            ans+=i-j+1;
         }
-        long long int it=end-start;
-        sum+=(long long int)(((it)*(it+1))/2);
-        return sum;
+        return ans;
     }
 };
