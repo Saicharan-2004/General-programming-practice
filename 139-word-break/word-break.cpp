48995ed1f87c5b7e887907f2b21474a995e0128c
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-05-25 17:22:16 +0530

    Time: 39 ms (6.75%) | Memory: 34.1 MB (5.04%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    struct hash_pair 
+    {
+        template <class T1, class T2>
+        size_t operator()(const pair<T1, T2>& p) const
+        {
+            auto hash1 = hash<T1>{}(p.first);
+            auto hash2 = hash<T2>{}(p.second);
+
+            if (hash1 != hash2) {
+                return hash1 ^ hash2;             
+            }
+            return hash1;
+        }
+    };
+    bool helper(unordered_set<string>words,string s,int ind,int n,vector<int>&dp)
+    {
+        if(ind==n)
+        {
+            return 1;
+        }
+        if(dp[ind]!=-1)
+        {
+            return dp[ind];
+        }
+        string temp="";
+        for(int j=ind;j<s.length();j++)
+        {
+            temp=temp+s[j];
+            if(words.find(temp)!=words.end())
+            {
+                if(helper(words,s,j+1,n,dp))
+                {
+                    return dp[ind]= true;
+                }
+            }
+        }
+        return dp[ind]= false;
+    }
+    bool wordBreak(string s, vector<string>& words) 
+    {
+        int n=words.size();
+        vector<int>dp(s.length()+1,-1);
+        unordered_set<string>rt(words.begin(),words.end());
+        return helper(rt,s,0,s.length(),dp);
+    }
+};
