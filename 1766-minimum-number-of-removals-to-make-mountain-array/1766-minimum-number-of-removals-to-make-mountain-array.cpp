0757c3378b47685d00986618e4406305d049ee99
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-06-01 13:11:57 +0530

    Time: 8 ms (97.01%), Space: 14.7 MB (35.56%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
-    int minimumMountainRemovals(vector<int>& nums) 
+    vector<int> findlis(vector<int> &nums)
+    {
+        vector<int> lis(nums.size(),0);
+        vector<int> vec;
+        vec.push_back(nums[0]); 
+        lis[0] = 1;
+        for(int i = 1 ; i < nums.size() ; i++)
+        {
+            if(nums[i] > vec.back())    
+                vec.push_back(nums[i]);
+            else
+            {
+                int idx = lower_bound(vec.begin() , vec.end() , nums[i]) - vec.begin();
+                vec[idx] = nums[i];
+            }
+            lis[i] = vec.size();
+        }
+        return lis;
+    }
+    int mysol(vector<int>& nums)
     {
         int n = nums.size();
         vector<int>lis(n);
@@ -43,5 +62,20 @@ public:
                 ans = max (ans , (lis[i] + lds[i] - 1));
         }
         return n-ans;
+    }
+    int minimumMountainRemovals(vector<int>& nums) 
+    {
+        vector<int> frontlis = findlis(nums);
+        reverse(nums.begin() , nums.end());
+        vector<int> revlis = findlis(nums);
+        reverse(revlis.begin() , revlis.end());
+        int maxlen = 0;
+        for(int i = 0 ; i < frontlis.size() ; i++)
+        {
+            if(frontlis[i] == 1 || revlis[i] == 1)  
+                continue;
+            maxlen = max(maxlen , frontlis[i] + revlis[i] - 1);
+        }
+        return nums.size() - maxlen;
     }
 };
