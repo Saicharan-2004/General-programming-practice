e624974b4c7d239812b175459ea5dff671e2a890
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-06-01 12:46:19 +0530

    Time: 5 ms (27.27%) | Memory: 7.8 MB (78.98%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.cpp
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int scoreOfString(string s) {
         int sum = 0;
-        for(int i = 0; i+1 <= s.size()-1;i++){
+        for(int i = 0; i < s.length()-1;i++){
             int a = s[i];
             int b = s[i+1];
             if (a > b){
                 sum += a-b;
             }
             else{
-                sum += b -a;
+                sum += b-a;
             }
         }
         return sum;
