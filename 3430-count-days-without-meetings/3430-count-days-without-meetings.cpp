ab1c1cf8c26ab8a2107c5d1b571610578389764d
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-06-06 15:58:28 +0530

    Time: 632 ms (5.01%), Space: 141.4 MB (22.65%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -4,54 +4,67 @@ public:
     {
         // unordered_set<int>temp;
         int n = meetings.size();
-        int ans = 0;
+        // int ans = 0;
         sort(meetings.begin(),meetings.end());
-        int beg=meetings[0][0];
-        int end=meetings[0][1];
-        vector<pair<int,int>>tp;
-        for(int i = 1;i<n;i++)
-        {
-            if(meetings[i][0] <= end + 1)
-            {
-                end = max(end,meetings[i][1]);
-            }
-            else
-            {
-                tp.push_back({beg,end});
-                beg = meetings[i][0];
-                end = meetings[i][1];
-            }
-        }
-        tp.push_back({beg,end});
-        int avail = 0;
-        // for(auto it:meetings)
+        // int beg=meetings[0][0];
+        // int end=meetings[0][1];
+        // vector<pair<int,int>>tp;
+        // for(int i = 1;i<n;i++)
         // {
-        //     cout<<it[0]<<" "<<it[1]<<":";
-        // }
-        // cout<<endl;
-        // for(int i = 0;i<n;i++)
-        // {
-        //     int j = i;
-        //     while(j<n-1 && meetings[j][1] >= meetings[j+1][0])
+        //     if(meetings[i][0] <= end + 1)
         //     {
-        //         j++;
+        //         end = max(end,meetings[i][1]);
         //     }
-        //     ans += meetings[j][1] - meetings[i][0] + 1;
-        //     i = j;
+        //     else
+        //     {
+        //         tp.push_back({beg,end});
+        //         beg = meetings[i][0];
+        //         end = meetings[i][1];
+        //     }
         // }
-        if(tp[0].first != 1)
+        // tp.push_back({beg,end});
+        // int avail = 0;
+        for(auto it:meetings)
         {
-            avail +=tp[0].first - 1;
+            cout<<it[0]<<" "<<it[1]<<":";
         }
-        for(int i = 1;i<tp.size();i++)
+        cout<<endl;
+        int ans = 0;
+        int prev = 1;
+        for(int i = 0;i<n;i++)
         {
-            avail +=  tp[i].first - tp[i-1].second - 1;
+            int start = meetings[i][0];
+            int end = meetings[i][1];
+            if(start > prev)
+            {
+                if(i == 0)
+                {
+                    ans += start - prev;
+                }
+                else
+                {
+                    ans += start - prev - 1;
+                }
+            }
+            prev = max(prev,end);
         }
-        if(tp[tp.size() - 1].second < days)
+        if(days > prev)
         {
-            avail += days - tp[tp.size() - 1].second;
+            ans +=days-prev;
         }
-        
-        return avail;
+        return ans;
+        // if(tp[0].first != 1)
+        // {
+        //     avail +=tp[0].first - 1;
+        // }
+        // for(int i = 1;i<tp.size();i++)
+        // {
+        //     avail +=  tp[i].first - tp[i-1].second - 1;
+        // }
+        // if(tp[tp.size() - 1].second < days)
+        // {
+        //     avail += days - tp[tp.size() - 1].second;
+        // }
+        // return avail;
     }
 };
