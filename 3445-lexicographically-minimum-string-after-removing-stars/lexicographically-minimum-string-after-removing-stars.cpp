322e64f7b7f3f36d3c53bc9fb2fa41683315cf0f
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-06-06 16:12:45 +0530

    Time: 319 ms (37.23%) | Memory: 119.3 MB (20.38%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     string clearStars(string s) {
-        set<pair<char, int>> all; // store sorted pair's <char, int> (ith character, -i)
-        int n = s.size();
-        
-        for(int i = 0; i < n; i++) {
-            if(s[i] == '*') all.erase(all.begin()); // encountered "*" then remove first left smallest character.
-            else all.insert(make_pair(s[i], -i));   // encountered non-"*" then add to set
+        set<pair<char,int>>tp;
+        int n = s.length();
+        string newstr = s;
+        for(int i = 0;i<n;i++)
+        {
+            if(s[i]!='*')
+            {
+                tp.insert({s[i],n-i});
+            }
+            else
+            {
+                auto r = tp.begin();
+                s[n-r->second]='*';
+                tp.erase(tp.begin());
+            }
         }
-        
-        string res(n, '$');        // here we have remaning characters with index range (0 - n)
-        for(auto &[ch, idx] : all)
-            res[abs(idx)] = ch;
-        
-        string ans = "";  // calculate our optimal answer
-        for(auto &r : res) 
-            if(r != '$') ans.push_back(r);
+        string ans = "";
+        for(auto it:s)
+        {
+            if(it != '*')   ans += it;
+        }
         return ans;
     }
 };
