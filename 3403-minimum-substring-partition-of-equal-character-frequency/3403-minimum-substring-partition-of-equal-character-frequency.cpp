be209b432d8670231ffeb14c7bac678387573741
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-06-10 01:39:35 +0530

    Time: 829 ms (52.07%), Space: 61.8 MB (38.45%) - LeetHub

--- 3403-minimum-substring-partition-of-equal-character-frequency/3403-minimum-substring-partition-of-equal-character-frequency.cpp
+++ 3403-minimum-substring-partition-of-equal-character-frequency/3403-minimum-substring-partition-of-equal-character-frequency.cpp
@@ -1,34 +1,70 @@
 class Solution {
-private:
-    int solve(vector<int>& dp,string & s,int idx){
-        if(idx==s.size()) return 0;
-        if(dp[idx]!=-1) return dp[idx];
-        unordered_map<char,int>mpp;
-        int ans=1e9,var=1e9;
-        for(int j=idx;j<s.length();j++){
-            mpp[s[j]]++;
-            int prevFreq = -1;
-            bool check=true;
-            for(auto it:mpp){
-                if(prevFreq == -1){
-                    prevFreq = it.second;
-                }
-                else if(prevFreq != it.second){
-                    check = false;
-                    break;
-                }
+public:
+    int helper(int ind, string s,vector<int>dp) {
+        if (ind < 0)
+            return 0;
+        int ans = s.size();
+        unordered_map<char,int>tp;
+        if(dp[ind]!=-1)
+        {
+            return dp[ind];
+        }
+        for (int i = ind; i >= 0; i--) 
+        {
+            tp[s[i]]++;
+            int maxi = INT_MIN;
+            int mini = INT_MAX;
+            for (auto it : tp) {
+                maxi = max(maxi, it.second);
+                mini = min(mini, it.second);
             }
-            if(check){
-                var=1+solve(dp,s,j+1);
-                ans=min(ans,var);
-                //cout<<ans<<" ";
+            // cout<<maxi<<" "<<mini<<" : ";
+            if (maxi == mini) 
+            {
+                ans = min(ans, 1 + helper(i - 1, s ,dp));
+            } 
+            else 
+            {
+                continue;
             }
         }
-        return dp[idx]=ans;
+        return dp[ind] = ans;
     }
-public:
     int minimumSubstringsInPartition(string s) {
-        vector<int>dp(s.size(),-1);
-        return solve(dp,s,0);
+        vector<int>dp(s.size()+1,0);
+        // return helper(s.size()-1, s,dp);
+        for(int ind = 0;ind<s.size();ind++)
+        {
+            int ans = s.size();
+            unordered_map<char,int>tp;
+            for (int i = ind; i >= 0; i--) 
+            {
+                tp[s[i]]++;
+                int maxi = INT_MIN;
+                int mini = INT_MAX;
+                for (auto it : tp) {
+                    maxi = max(maxi, it.second);
+                    mini = min(mini, it.second);
+                }
+                // cout<<maxi<<" "<<mini<<" : ";
+                if (maxi == mini) 
+                {
+                    if(i > 0)
+                    {
+                        ans = min(ans,1+dp[i-1]);
+                    }
+                    else
+                    {
+                        ans = min(ans,1);
+                    }
+                } 
+                else 
+                {
+                    continue;
+                }
+            }
+            dp[ind] = ans;
+        }
+        return dp[s.size() - 1];
     }
 };
