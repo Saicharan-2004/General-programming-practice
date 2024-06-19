6b043777b09f3b886197988e648d5d8eaaef5125
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-06-19 15:55:34 +0530

    Time: 156 ms (5.04%), Space: 121.3 MB (7.7%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minDays(vector<int>& arr, int m, int k) {
         int n = arr.size();
-        if((long)n < (long)m*k) return -1;
+        if((long)n <(long) m*k) return -1;
         int maxi = -1;
         int mini = INT_MAX;
         for(auto it:arr)
@@ -12,29 +12,35 @@ public:
         }
         int start = mini;
         int end = maxi;
-        int ans = INT_MAX;
+        int ans =INT_MAX;
         while(start<=end)
         {
             int mid = start + (end-start)/2;
             int counter = 0;
-            int num = 0;
+            vector<int>boolchecker(n,false);
             for(int i = 0;i<n;i++)
             {
                 if(arr[i] <= mid)
                 {
-                    counter++;
-                }
-                else
-                {
-                    counter = 0;
-                }
-                if(counter == k)
-                {
-                    num++;
-                    counter = 0;
+                    boolchecker[i] = true;
                 }
             }
-            if(num < m)
+            int i = 0;
+            while(i<arr.size())
+            {
+                int cnt = 0;
+                if(boolchecker[i])
+                {
+                    while(i<n && boolchecker[i] == true)
+                    {
+                        cnt++;
+                        i++;
+                    }
+                }
+                counter += cnt/k;
+                i++;
+            }
+            if(counter < m)
             {
                 start = mid + 1;
             }
