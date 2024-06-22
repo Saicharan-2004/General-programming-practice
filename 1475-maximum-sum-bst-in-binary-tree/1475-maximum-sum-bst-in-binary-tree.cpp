4b5ce441bab0299129b8dd27fdc815f90cc8ab95
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-06-22 13:59:23 +0530

    Time: 130 ms (87.62%), Space: 100.9 MB (92.94%) - LeetHub

---
+++ 1475-maximum-sum-bst-in-binary-tree/1475-maximum-sum-bst-in-binary-tree.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ class helper{
+    public:
+    int sum;
+    int mini;
+    int maxi;
+    helper(int sum,int mini,int maxi)
+    {
+        this->sum = sum;
+        this->mini = mini;
+        this->maxi = maxi;
+    }
+ };
+class Solution {
+public:
+    int summer = INT_MIN;
+    helper inorder(TreeNode * root)
+    {
+        if(root == NULL)
+        {
+            return helper(0,INT_MAX,INT_MIN);
+        }
+        helper leftT = inorder(root->left);
+        helper rightT = inorder(root->right);
+        if(leftT.maxi < root->val && root->val < rightT.mini)
+        {
+            summer = max(summer,leftT.sum + rightT.sum + root->val);
+            return helper(leftT.sum + rightT.sum + root->val,min(leftT.mini,root->val),max(rightT.maxi,root->val));
+        }
+        summer = max(summer,max(leftT.sum,rightT.sum));
+        return helper(max(leftT.sum,rightT.sum),INT_MIN,INT_MAX);
+    }
+    int maxSumBST(TreeNode* root) {
+        auto ans = inorder(root);
+        return max(0,summer);
+    }
+};
