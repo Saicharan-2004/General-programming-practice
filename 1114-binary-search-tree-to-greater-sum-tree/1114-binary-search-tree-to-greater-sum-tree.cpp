e87a39f43d296898ea384df53dd34acef3f8c189
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-06-25 12:55:57 +0530

    Time: 2 ms (49.63%), Space: 10.2 MB (42.71%) - LeetHub

--- 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.cpp
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.cpp
@@ -12,41 +12,54 @@
 class Solution {
 public:
     TreeNode* bstToGst(TreeNode* root) {
-        TreeNode * curr = root;
-        vector<TreeNode*>inorder;
-        while(curr != NULL)
-        {
-            if(curr->left == NULL)
-            {
-                inorder.push_back(curr);
-                curr = curr->right;
-            }
-            else
-            {
-                TreeNode * prev = curr->left;
-                while(prev->right!= NULL && prev->right!=curr)
-                {
-                    prev = prev->right;
-                }
-                if(prev ->right == NULL)
-                {
-                    prev->right = curr;
-                    curr = curr->left;
-                }
-                else
-                {
-                    prev->right = NULL;
-                    inorder.push_back(curr);
-                    curr = curr->right;
-                }
-            }
+            int acc_sum = 0;
+            sum_node(root,acc_sum);
+            return root;
         }
-        int prefix = inorder.back()->val;
-        for(int i = inorder.size()-2;i>=0;i--)
-        {
-            inorder[i]->val = inorder[i]->val + prefix;
-            prefix = inorder[i]->val;
-        }
-        return root;
+        
+    void sum_node(TreeNode* node, int& acc_sum){
+        if (node == NULL)  return;
+        sum_node(node->right,acc_sum);
+        node->val += acc_sum;
+        acc_sum = node->val;
+        sum_node(node->left, acc_sum);
     }
+    // TreeNode* bstToGst(TreeNode* root) {
+    //     TreeNode * curr = root;
+    //     vector<TreeNode*>inorder;
+    //     while(curr != NULL)
+    //     {
+    //         if(curr->left == NULL)
+    //         {
+    //             inorder.push_back(curr);
+    //             curr = curr->right;
+    //         }
+    //         else
+    //         {
+    //             TreeNode * prev = curr->left;
+    //             while(prev->right!= NULL && prev->right!=curr)
+    //             {
+    //                 prev = prev->right;
+    //             }
+    //             if(prev ->right == NULL)
+    //             {
+    //                 prev->right = curr;
+    //                 curr = curr->left;
+    //             }
+    //             else
+    //             {
+    //                 prev->right = NULL;
+    //                 inorder.push_back(curr);
+    //                 curr = curr->right;
+    //             }
+    //         }
+    //     }
+    //     int prefix = inorder.back()->val;
+    //     for(int i = inorder.size()-2;i>=0;i--)
+    //     {
+    //         inorder[i]->val = inorder[i]->val + prefix;
+    //         prefix = inorder[i]->val;
+    //     }
+    //     return root;
+    // }
 };
