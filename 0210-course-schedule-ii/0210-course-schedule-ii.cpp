0b3c4f72ff10728ecfc58ba4c4eb5597f471cd17
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-06-28 13:01:59 +0530

    Time: 18 ms (41.13%), Space: 17.6 MB (37.85%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -34,13 +34,40 @@ public:
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>adj(numCourses);
+        vector<int>indegree(numCourses,0);
         for(auto it:prerequisites)
         {
             adj[it[1]].push_back(it[0]);
+            indegree[it[0]]++;
         }
-        isCyclic(numCourses,adj);
+        queue<int>q;
+        vector<int>ans;
+        for(int i = 0;i<numCourses;i++)
+        {
+            if(indegree[i] == 0)    
+            {
+                q.push(i);
+                ans.push_back(i);
+            }
+        }
+        while(!q.empty())
+        {
+            int front = q.front();
+            q.pop();
+            for(auto it:adj[front])
+            {
+                indegree[it]--;
+                if(indegree[it] == 0)
+                {
+                    q.push(it);
+                    ans.push_back(it);   
+                }
+            }
+        }
+        // isCyclic(numCourses,adj);
         if(ans.size() != numCourses)    return {};
-        reverse(ans.begin(),ans.end());
+        // reverse(ans.begin(),ans.end());
         return ans;
+
     }
 };
