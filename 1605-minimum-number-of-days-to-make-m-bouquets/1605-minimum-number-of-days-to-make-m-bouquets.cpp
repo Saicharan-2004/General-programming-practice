6043b3f3c87e0d60b11671e113b3da6f913e1c9b
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-07-09 22:11:01 +0530

    Time: 107 ms (83.52%), Space: 69.1 MB (26.9%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -2,54 +2,41 @@ class Solution {
 public:
     int minDays(vector<int>& arr, int m, int k) {
         int n = arr.size();
-        if((long)n <(long) m*k) return -1;
-        int maxi = -1;
-        int mini = INT_MAX;
-        for(auto it:arr)
+        if(n<long(m)*k)
         {
-            maxi = max(maxi,it);
-            mini = min(mini,it);
+            return -1;
         }
-        int start = mini;
-        int end = maxi;
-        int ans =INT_MAX;
+        int end = *max_element(arr.begin(),arr.end());
+        int start = *min_element(arr.begin(),arr.end());
+        // cout<<start<<" "<<end<<endl;
         while(start<=end)
         {
-            int mid = start + (end-start)/2;
-            int counter = 0;
-            vector<int>boolchecker(n,false);
+            int mid = (end+start)/2;
+            int numBouquets = 0;
+            int numFlowers = 0;
             for(int i = 0;i<n;i++)
             {
                 if(arr[i] <= mid)
                 {
-                    boolchecker[i] = true;
+                    numFlowers++;
                 }
-            }
-            int i = 0;
-            while(i<arr.size())
-            {
-                int cnt = 0;
-                if(boolchecker[i])
+                else
                 {
-                    while(i<n && boolchecker[i] == true)
-                    {
-                        cnt++;
-                        i++;
-                    }
+                    numBouquets += int(numFlowers/k);
+                    numFlowers = 0;
                 }
-                counter += cnt/k;
-                i++;
             }
-            if(counter < m)
+            numBouquets += int(numFlowers/k);
+            if(numBouquets<m)
             {
                 start = mid + 1;
             }
             else
             {
-                ans = min(ans , mid);
+                // ans = min(ans,mid);
                 end = mid - 1;
             }
         }
-        return ans;
+        return end+1;
     }
 };
