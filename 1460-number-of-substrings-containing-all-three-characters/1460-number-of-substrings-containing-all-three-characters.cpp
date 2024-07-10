6a1c7fd9bbb9d3d26b38774a2a1575bd30e36989
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-07-10 22:07:53 +0530

    Time: 59 ms (24.9%), Space: 16.4 MB (15.17%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-    int numberOfSubstrings(string s) 
-    {
+    int numberOfSubstrings(string s) {
         int n = s.size();
-        unordered_map<char,int>tp;
-        int right = 0;
-        int left = 0;
+        int l = 0;
+        int r = 0;
         int ans = 0;
-        while(right<n)
+        unordered_map<char,int>mpp;
+        while(r<n)
         {
-            tp[s[right]]++;
-            // if(tp.size() == 3)
-            // {
-            //     ans += (n-right);
-                while(tp.size()>=3)
+            mpp[s[r]]++;
+            while(mpp.size() == 3)
+            {
+                ans += n-r;
+                mpp[s[l]]--;
+                if(mpp[s[l]] == 0)
                 {
-                    if(tp.size()>=3)
-                    {
-                        ans += n-right;
-                    }
-                    tp[s[left]]--;
-                    if(tp[s[left]]==0)
-                    {
-                        tp.erase(s[left]);
-                    }
-                    left++;
+                    mpp.erase(s[l]);
                 }
-            // }
-            right++;
+                l++;
+            }
+            r++;
         }
         return ans;
     }
