e4dca6d0c8dfb40cd51519822dab33190519a838
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-07-14 11:37:46 +0530

    Time: 4 ms (75.00%) | Memory: 29.7 MB (100.00%) - LeetSync

---
+++ 3494-minimum-cost-for-cutting-cake-i/minimum-cost-for-cutting-cake-i.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int helper(int n,int m,vector<int>hori,vector<int>verti)
+    {
+        int vertiCount = 1;
+        int horiCount = 1;
+        sort(verti.begin(),verti.end());
+        sort(hori.begin(),hori.end());
+        int i = m-2;
+        int j = n-2;
+        int ans = 0;
+        while(i>=0 && j>=0)
+        {
+            if(hori[i]>verti[j])
+            {
+                ans+=hori[i]*horiCount;
+                vertiCount++;
+                i--;
+            }
+            else
+            {
+                ans+=verti[j]*vertiCount;
+                horiCount++;
+                j--;
+            }
+        }
+        while(i>=0)
+        {
+            ans+=hori[i]*horiCount;
+            // vertiCount++;
+            i--;
+        }
+        while(j>=0)
+        {
+            ans+=verti[j]*vertiCount;
+            // horiCount++;
+            j--;
+        }
+        return ans;
+    }
+    int minimumCost(int m, int n, vector<int>& horizontalCut, vector<int>& verticalCut) 
+    {
+        return helper(n,m,horizontalCut,verticalCut);
+    }
+};
