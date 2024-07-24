c57cbebb5b70ccdaec0b5a2c927ddc6ede44fb8c
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-07-24 17:31:57 +0530

    Time: 19 ms (96.85%) | Memory: 33.7 MB (83.94%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,54 +1,69 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        // int n = arr.size();
+        // int lower = lower_bound(arr.begin(),arr.end(),x)-arr.begin();
+        // if(lower == 0)
+        // {
+        //     return vector<int>(arr.begin(),arr.begin()+k);
+        // }
+        // else if(lower == n)
+        // {
+        //     return vector<int>(arr.begin()+n-k,arr.end());
+        // }
+        // else
+        // {
+        //     int ptr1 = lower;
+        //     int ptr2 = lower + 1;
+        //     int count = 0;
+        //     vector<int>ans;
+        //     // ans.push_back(arr[lower]);
+        //     while(count<k)
+        //     {
+        //         if(ptr1>=0 && ptr2<n)
+        //         {
+        //             if(x-arr[ptr1]<=arr[ptr2]-x)
+        //             {
+        //                 ans.push_back(arr[ptr1]);
+        //                 ptr1--;
+        //             }
+        //             else
+        //             {
+        //                 ans.push_back(arr[ptr2]);
+        //                 ptr2++;
+        //             }
+        //         }
+        //         else if(ptr1>=0)
+        //         {
+        //             ans.push_back(arr[ptr1]);
+        //             ptr1--;
+        //         }
+        //         else if(ptr2<n)
+        //         {
+        //             ans.push_back(arr[ptr2]);
+        //             ptr2++;
+        //         }
+        //         count++;
+        //     }
+        //     sort(ans.begin(),ans.end());
+        //     return ans;
+        // }
+        // return {};
         int n = arr.size();
-        int lower = lower_bound(arr.begin(),arr.end(),x)-arr.begin();
-        if(lower == 0)
-        {
-            return vector<int>(arr.begin(),arr.begin()+k);
-        }
-        else if(lower == n)
-        {
-            return vector<int>(arr.begin()+n-k,arr.end());
-        }
-        else
-        {
-            int ptr1 = lower - 1;
-            int ptr2 = lower;
-            int count = 0;
-            vector<int>ans;
-            // ans.push_back(arr[lower]);
-            while(count<k)
-            {
-                if(ptr1>=0 && ptr2<n)
-                {
-                    if(x-arr[ptr1]<=arr[ptr2]-x)
-                    {
-                        ans.push_back(arr[ptr1]);
-                        ptr1--;
-                    }
-                    else
-                    {
-                        ans.push_back(arr[ptr2]);
-                        ptr2++;
-                    }
-                }
-                else if(ptr1>=0)
-                {
-                    ans.push_back(arr[ptr1]);
-                    ptr1--;
-                }
-                else if(ptr2<n)
-                {
-                    ans.push_back(arr[ptr2]);
-                    ptr2++;
-                }
-                count++;
+        int left = 0, right = n - 1;
+
+        // Narrow down the window to size k
+        while (right - left >= k) {
+            if (abs(arr[left] - x) > abs(arr[right] - x)) {
+                left++;
+            } else {
+                right--;
             }
-            sort(ans.begin(),ans.end());
-            return ans;
         }
-        return {};
+
+        // Collect the result
+        vector<int> ans(arr.begin() + left, arr.begin() + left + k);
+        return ans;
     }
     // vector<int> findClosestElements(vector<int>& arr, int k, int x) {
     //     int n = arr.size();
