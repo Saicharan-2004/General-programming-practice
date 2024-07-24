00462f787aea0476925ae9c82042e8085237ebb1
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-07-24 14:31:04 +0530

    Time: 16 ms (25.88%), Space: 21.3 MB (12.09%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.cpp
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     int hIndex(vector<int>& arr) {
-        // int start = 0;
-        // int end = arr.size()-1;
-        // int n = end+1;
+        int start = 0;
+        int end = arr.size()-1;
+        int n = end+1;
         // int ans = 0;
-        // while(start<=end)
+        while(start<=end)
+        {
+            int mid = start + (end-start)/2;
+            if(n-mid>arr[mid])
+            {
+                start = mid + 1;
+                // ans = max(ans,n-mid);
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+        return n-start;
+        // int ans = 0;
+        // int n = arr.size();
+        // for(int i = 0;i<=1000;i++)
         // {
-        //     int mid = start + (end-start)/2;
-        //     if(n-mid>=arr[mid])
+        //     int num = lower_bound(arr.begin(),arr.end(),i)-arr.begin();
+        //     if(n-num >= i)
         //     {
-        //         start = mid + 1;
-        //         ans = max(ans,n-mid);
-        //     }
-        //     else
-        //     {
-        //         end = mid - 1;
+        //         ans = max(ans,i);
         //     }
         // }
         // return ans;
-        int ans = 0;
-        int n = arr.size();
-        for(int i = 0;i<=1000;i++)
-        {
-            int num = lower_bound(arr.begin(),arr.end(),i)-arr.begin();
-            if(n-num >= i)
-            {
-                ans = max(ans,i);
-            }
-        }
-        return ans;
         // if(end == -1)
         // {
         //     return n;
