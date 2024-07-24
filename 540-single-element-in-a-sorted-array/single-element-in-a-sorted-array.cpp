e7bc8c4d758409531463428b71727254b80206db
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-07-24 14:59:49 +0530

    Time: 27 ms (6.19%) | Memory: 25 MB (12.69%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& arr) {
-        int n = arr.size();
-        if(n==1)
-        {
-            return arr[0];
-        }
-        if(arr[0]!=arr[1])
-        {
-            return arr[0];
-        }
-        if(arr[n-1]!=arr[n-2])
-        { 
-            return arr[n-1];
-        }
-        int start = 1;
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1)  return nums[0];
+        if(nums[0] != nums[1])  return nums[0];
+        if(nums[n-1]!=nums[n-2])    return nums[n-1];
+        int start=0;
         int end = n-2;
         while(start<=end)
         {
             int mid = start + (end-start)/2;
-            if(arr[mid-1]!=arr[mid] && arr[mid]!=arr[mid+1])
+            if(nums[mid-1]!=nums[mid] && nums[mid] != nums[mid + 1])
             {
-                return arr[mid];
+                return nums[mid];
             }
-            if((mid%2==1 && arr[mid] == arr[mid-1]) || (mid%2 == 0 && arr[mid] == arr[mid+1]))
+            if(mid%2)
             {
-                start = mid + 1;
+                if(nums[mid] == nums[mid - 1])
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
             }
             else
             {
-                end = mid - 1;
+                if(nums[mid] == nums[mid+1])
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
             }
         }
         return -1;
