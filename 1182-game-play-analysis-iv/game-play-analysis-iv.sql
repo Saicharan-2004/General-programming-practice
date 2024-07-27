9170e8052a7539104681b20783efed18b0daf6e2
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-07-27 12:30:42 +0530

    Time: 631 ms (41.25%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select round(count(distinct tab.player_id)/count(distinct act.player_id),2) as fraction
+from activity as act
+left join
+(select player_id,min(event_date) as mini
+from Activity
+group by player_id) as tab
+on act.player_id = tab.player_id
+and date_sub(act.event_date,interval 1 day) = tab.mini
