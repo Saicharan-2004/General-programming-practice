dca582a03d747f62f597fbd39491832cb8c438d2
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-07-30 16:57:48 +0530

    Time: 97 ms (95.72%) | Memory: 51.5 MB (84.28%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
-    vector<int>fin;
-    bool dfs(int start,vector<vector<int>> &adj,vector<int>&visited)
+    vector<int>ans;
+    bool dfs(int node,vector<int>&visited,vector<vector<int>>& adj)
     {
-        visited[start] = 2;
-        for(auto it:adj[start])
+        visited[node] = 2;
+        for(auto it:adj[node])
         {
             if(visited[it] == 0)
             {
-                if(dfs(it,adj,visited))  
+                if(dfs(it,visited,adj))
+                {
                     return true;
+                }
             }
             else if(visited[it] == 2)
             {
                 return true;
             }
         }
-        fin.push_back(start);
-        visited[start] = 1;
+        ans.push_back(node);
+        visited[node] = 1;
         return false;
     }
-    bool isCyclic(int V, vector<vector<int>> &adj)
+    vector<int> eventualSafeNodes(vector<vector<int>>& adj) 
     {
-        vector<int>visited(V,0);
-        for(int i = 0;i<V;i++)
+        int n = adj.size();
+        vector<int>visited(n,false);
+        for(int i = 0;i<n;i++)
         {
             if(visited[i] == 0)
             {
-                bool ans = dfs(i,adj,visited);
+                dfs(i,visited,adj);
             }
         }
-        return false;
-    }
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int>safe;
-        isCyclic(n,graph);
-        sort(fin.begin(),fin.end());//can avoid this;
-        return fin;
+        sort(ans.begin(),ans.end());
+        return ans;
     }
 };
