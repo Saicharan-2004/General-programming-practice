e82e4ab07f8ae18f7d6bf083b416400d2c3611c3
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-07-30 16:43:59 +0530

    Time: 28 ms (15.84%), Space: 20 MB (14.53%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>&isConnected,vector<int>&visited,int node)
-    {
-        visited[node] = true;
-        int n = isConnected.size();
-        for(int i = 0;i<n;i++)
-        {
-            if(node != i && visited[i] == false && isConnected[node][i] == 1)
-            {
-                dfs(isConnected,visited,i);
-            }
-        }
-    }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        // vector<vector<int>>adj(n+1);
-        // for(int i = 0;i<n;i++)
-        // {
-        //     for(int j = 0;j<n;j++)
-        //     {
-        //         if(isConnected[i][j] == true)
-        //             adj[i].push_back(j);
-        //     }
-        // }
+        vector<vector<int>>adj(n);
+        for(int i = 0;i<n;i++)
+        {
+            for(int j = 0;j<n;j++)
+            {
+                if(isConnected[i][j] == true)
+                    adj[i].push_back(j);
+            }
+        }
         int count =  0;
         vector<int>visited(n,false);
         for(int i=0;i<n;i++)
         {
+            int curr = i;
             if(visited[i] == false)
             {
-                dfs(isConnected,visited,i);
+                queue<int>q;
+                q.push(curr);
+                while(!q.empty())
+                {
+                    int front = q.front();
+                    q.pop();
+                    visited[front] = true;
+                    for(auto it:adj[front])
+                    {
+                        if(visited[it] == false)
+                        {
+                            q.push(it);
+                        }
+                    }
+                }
                 count++;
             }
         }
